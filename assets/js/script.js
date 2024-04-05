3009dac0d56f7b68a416a68c366b5aded19bc63d
Aynur <ghostgo81@gmail.com>
2024-04-05 17:40:26 +0400

    fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -165,43 +165,15 @@ window.addEventListener('DOMContentLoaded', () => {
         advantagesSegments[currentAdvantagesList].classList.add('navigation-block__segment--active');
     });
 
+    // const mobileMql = window.matchMedia('(max-width: 1110px)');
+    // const parallaxElements = document.querySelectorAll('.mouse_parallax');
 
-    //! IMG PARALLAX
-
-    const mobileMql = window.matchMedia('(max-width: 1110px)');
-    const parallaxElements = document.querySelectorAll('.mouse_parallax');
-
-    if (!mobileMql.matches)
-    for (let i = 0; i < parallaxElements.length; i++) {
-        window.addEventListener('mousemove', function (e) {
-        let x = e.clientX / window.innerWidth;
-        let y = e.clientY / window.innerHeight;
-        parallaxElements[i].style.transform = 'translate(-' + x * 15 + 'px, -' + y * 15 + 'px)';
-        });
-    }
-
-    // menuBtn = document.querySelector('.menu__btn'),
-    // menuList = document.querySelector('.menu__list'),
-    // menuBtnImg = document.querySelector('.menu__btn img'),
-    // menuPopup = document.querySelector('.menu__popup'),
-    // orderBtn = document.querySelectorAll('.solutions__toggle-popup'),
-    // formPopup = document.querySelector('.solutions__popup');
-
-    // menuBtn.addEventListener('click', () => {
-    // if (mobileMql.matches)
-    //     menuPopup.classList.toggle('menu__popup--active');
-    // else
-    //     menuList.classList.toggle('menu__list--active');
-
-    // if (menuList.classList.contains('menu__list--active') || menuPopup.classList.contains('menu__popup--active'))
-    //     menuBtnImg.src = '../assets/icons/menuClose.svg';
-    // else
-    //     menuBtnImg.src = '../assets/icons/menu.svg';
-    // });
-
-    // for (let i = 0; i < orderBtn.length; i++) {
-    // orderBtn[i].addEventListener('click', () => {
-    //     formPopup.classList.toggle('solutions__popup--active');
-    // });
+    // if (!mobileMql.matches)
+    // for (let i = 0; i < parallaxElements.length; i++) {
+    //     window.addEventListener('mousemove', function (e) {
+    //     let x = e.clientX / window.innerWidth;
+    //     let y = e.clientY / window.innerHeight;
+    //     parallaxElements[i].style.transform = 'translate(-' + x * 15 + 'px, -' + y * 15 + 'px)';
+    //     });
     // }
 })
